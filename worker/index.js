cf3445074ad186dbba9690c7b1d848922e913f6f
Thomas Egli <thegli.apps@gmail.com>
2019-11-12 21:53:50 +0100

    initial commit

---
+++ worker/index.js
@@ -0,0 +1,25 @@
+const keys = require('./keys');
+const redis = require('redis');
+
+const redisClient = redis.createClient({
+  host: keys.redisHost,
+  port: keys.redisPort,
+  retry_stragety: () => 1000
+});
+
+const sub = redisClient.duplicate();
+
+function fib(index) {
+  if (index < 2) return 1;
+  return fib(index - 1) + fib(index - 2);
+}
+
+// subscription, message is the Fibonacci index
+// the Express App publishes the index, which we receive here
+sub.on('message', (channel, message) => {
+  console.log('Received new message: ' + message);
+  // stored the calculated Fibonnaci number (replacind the 'Nothing yet')
+  redisClient.hset('values', message, fib(message));
+});
+// trigger: when anything (=Fib index) is inserted, then perform above calcualation
+sub.subscribe('insert');
